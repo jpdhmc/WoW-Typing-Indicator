f9f99ff6182a5731d44140d9fa48416e75a44659
jpdhmc <112502289+jpdhmc@users.noreply.github.com>
2024-08-08 14:49:31 -0500

    slash commands start and stop tracking

--- main.lua
+++ main.lua
@@ -1,12 +1,25 @@
-prevText = ""
-
-local timerVar = C_Timer.NewTicker(1, function()
-    if ACTIVE_CHAT_EDIT_BOX ~= nil then
-        if ACTIVE_CHAT_EDIT_BOX:GetText() ~= "" and ACTIVE_CHAT_EDIT_BOX:GetText() ~= prevText then
-            print("You are typing: " .. ACTIVE_CHAT_EDIT_BOX:GetText())
-        else
-            print("You are not typing.")
-        end
-        prevText = ACTIVE_CHAT_EDIT_BOX:GetText()
+function toggleIndicator()
+    if timerVar ~= nil and not timerVar:IsCancelled() then
+        timerVar:Cancel()
+        print("not tracking typing")
+    else
+        prevText = ""
+        print("starting to track typing")
+        timerVar = C_Timer.NewTicker(1, function()
+            if ACTIVE_CHAT_EDIT_BOX ~= nil then
+                if ACTIVE_CHAT_EDIT_BOX:GetText() ~= "" and ACTIVE_CHAT_EDIT_BOX:GetText() ~= prevText then
+                    print("You are typing: " .. ACTIVE_CHAT_EDIT_BOX:GetText())
+                else
+                    print("You are not typing.")
+                end
+                prevText = ACTIVE_CHAT_EDIT_BOX:GetText()
+            end
+        end)
     end
-end)
+end
+
+-- slash commands
+SLASH_WOWTYPINGINDICATOR1, SLASH_WOWTYPINGINDICATOR2 = "/wowtypingindicator", "/wti";
+SlashCmdList["WOWTYPINGINDICATOR"] = function()
+    toggleIndicator()
+end
