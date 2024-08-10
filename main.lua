2193d3131ea270b677b5fbe6aa2b0a77a4cedec4
jpdhmc <112502289+jpdhmc@users.noreply.github.com>
2024-08-10 00:37:11 -0500

    individual indicator listeners ready for party test

--- main.lua
+++ main.lua
@@ -1,12 +1,12 @@
 local addonPrefix = "typingindicator"
 
 if C_ChatInfo.RegisterAddonMessagePrefix(addonPrefix) then
-    print("To enable WoW Typing Indicator, type /wowtypingindicator or /wti.")
+    print("To enable/disable WoW Typing Indicator, type /wowtypingindicator or /wti.")
 else
     print("WTI Addon prefix not registered properly.")
 end
 
--- party members
+-- set up party members
 local wtiPartyFrame = CreateFrame("Frame")
 local wtiGroupMembers = {}
 wtiPartyFrame:RegisterEvent("GROUP_ROSTER_UPDATE")
@@ -15,28 +15,27 @@ wtiPartyFrame:SetScript("OnEvent", function()
         -- this is because msgSender and unitfullname are formatted differently
         formattedUnitName = string.gsub(UnitFullName("party" .. i), " ", "-")
         wtiGroupMembers[formattedUnitName] = i
-        print(formattedUnitName)
-        print(wtiGroupMembers[formattedUnitName])
 
+        -- visuals
         wtiIndividualIndicator = CreateFrame("Frame", formattedUnitName .. "Frame", UIParent)
+        wtiIndividualIndicator:Hide()
         wtiIndividualIndicator:SetPoint("LEFT", _G["PartyFrame"]["MemberFrame" .. i], "RIGHT")
-        wtiIndividualIndicator:SetSize(64, 64)
+        wtiIndividualIndicator:SetSize(48, 48)
         wtiIndividualIndicator.tex = wtiIndividualIndicator:CreateTexture()
         wtiIndividualIndicator.tex:SetAllPoints(wtiIndividualIndicator)
         wtiIndividualIndicator.tex:SetTexture("interface/icons/ACHIEVEMENT_GUILDPERK_GMAIL")
-    end
-end)
 
-local wtiMessageFrame = CreateFrame("Frame", nil, UIParent, "BackdropTemplate")
-local wtiText = wtiMessageFrame:CreateFontString(nil, "OVERLAY", "GameTooltipText")
---wtiMessageFrame:SetPoint("LEFT", _G["PartyFrame"]["MemberFrame1"], "RIGHT")
-wtiMessageFrame:SetSize(150, 40)
-wtiMessageFrame:SetBackdrop(BACKDROP_TUTORIAL_16_16)
-wtiText:SetPoint("CENTER")
-wtiMessageFrame:RegisterEvent("CHAT_MSG_ADDON")
-wtiMessageFrame:SetScript("OnEvent", function(self, event, msgPrefix, msgText, msgChannel, msgSender)
-    if msgText == "1" then
-        wtiText:SetText(msgSender)
+        -- event
+        wtiIndividualIndicator:RegisterEvent("CHAT_MSG_ADDON")
+        wtiIndividualIndicator:SetScript("OnEvent", function(self, event, msgPrefix, msgText, msgChannel, msgSender)
+        if msgSender == formattedUnitName then
+            if msgText == "1" then
+                wtiIndividualIndicator:Show()
+            else
+                wtiIndividualIndicator:Hide()
+            end
+        end
+        end)
     end
 end)
 
@@ -51,10 +50,15 @@ local function toggleIndicator()
             if ACTIVE_CHAT_EDIT_BOX ~= nil then
                 if ACTIVE_CHAT_EDIT_BOX:GetText() ~= "" and ACTIVE_CHAT_EDIT_BOX:GetText() ~= prevText then
                     C_ChatInfo.SendAddonMessage(addonPrefix, "1", "INSTANCE_CHAT")
+                    print("sent 1")
                 else
                     C_ChatInfo.SendAddonMessage(addonPrefix, "0", "INSTANCE_CHAT")
+                    print("sent 0")
                 end
                 prevText = ACTIVE_CHAT_EDIT_BOX:GetText()
+            else
+                C_ChatInfo.SendAddonMessage(addonPrefix, "0", "INSTANCE_CHAT")
+                print("sent 0")
             end
         end)
     end
